e3520bd2f78cd68b6c3e239ba0deb4ba1d872978
aajuvale <ahmed.juvale@gmail.com>
2025-07-30 21:17:26 -0700

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Ahmed Juvale on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
