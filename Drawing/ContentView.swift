ab8653de744faa87e16251bbc626d11cf5a27a52
aajuvale <ahmed.juvale@gmail.com>
2025-08-03 20:15:55 -0700

    feat: implemented capsule with ImagePaint background)

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,45 +7,16 @@
 
 import SwiftUI
 
-struct Flower: Shape {
-    var petalOffest = -20.0
-    var petalWidth = 100.0
-
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-
-        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
-            let rotation = CGAffineTransform(rotationAngle: number)
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width/2, y: rect.height/2))
-
-            let originalPetal = Path(ellipseIn: CGRect(x: petalOffest, y: 0, width: petalWidth, height: rect.width / 2))
-            let rotatedPetal = originalPetal.applying(position)
-
-            path.addPath(rotatedPetal)
-        }
-
-        return path
-    }
-}
-
 struct ContentView: View {
-    @State
-    private var petalOffset = -20.0
-    @State
-    private var petalWidth = 100.0
 
     var body: some View {
-        VStack {
-            Flower(petalOffest: petalOffset, petalWidth: petalWidth)
-                .fill(.yellow, style: FillStyle(eoFill: true))
-
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
-
-            Text("Width")
-            Slider(value: $petalWidth, in: 0...100)
-                .padding(.horizontal)
-        }
+        Capsule()
+            .strokeBorder(ImagePaint(image: Image("example"), sourceRect: CGRect(
+                x: 0,
+                y: 0.25,
+                width: 1,
+                height: 0.5
+            ), scale: 0.3), lineWidth: 20)
+            .frame(width: 300, height: 200)
     }
 }
