7e266de6108fd12366d72735cbada456919fd5c7
aajuvale <ahmed.juvale@gmail.com>
2025-08-03 20:43:04 -0700

    feat: implemented blurs

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,50 +7,24 @@
 
 import SwiftUI
 
-struct ColorCyclingCircle: View {
-    var amount = 0.0
-    var steps = 100
-
-    var body: some View {
-        ZStack {
-            ForEach(0..<steps) { value in
-                Circle()
-                    .inset(by: Double(value))
-                    .strokeBorder(
-                        LinearGradient(
-                            gradient: Gradient(colors: [
-                                color(for: value, brightness: 1),
-                                color(for: value, brightness: 0.5)
-                            ]), startPoint: .top, endPoint: .bottom
-                        ),
-                        lineWidth: 2
-                    )
-            }
-        }
-        .drawingGroup() // Uses apple metal to use GPU graphics
-    }
-
-    func color(for value: Int, brightness: Double) -> Color {
-        var targetHue = Double(value) / Double(steps) + amount
-
-        if targetHue > 1 {
-            targetHue -= 1
-        }
-
-        return Color(hue: targetHue, saturation: 1, brightness: brightness)
-    }
-}
-
 struct ContentView: View {
     @State
-    private var colorCycle = 0.0
+    private var amount = 0.0
 
     var body: some View {
         VStack {
-            ColorCyclingCircle(amount: colorCycle)
-                .frame(width: 300, height: 300)
+            Image("example")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 400, height: 300)
+                .saturation(amount)
+                .blur(radius: (1 - amount) * 20)
 
-            Slider(value: $colorCycle)
+            Slider(value: $amount)
+                .padding()
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.black)
+        .ignoresSafeArea()
     }
 }
