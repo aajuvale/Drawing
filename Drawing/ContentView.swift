6f447c4d606a22aac877f983dd14a699cf39fedf
aajuvale <ahmed.juvale@gmail.com>
2025-08-03 20:08:10 -0700

    feat: implemented flower drawing program

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,58 +7,45 @@
 
 import SwiftUI
 
-struct Triangle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.closeSubpath()
-        
-        return path
-    }
-}
+struct Flower: Shape {
+    var petalOffest = -20.0
+    var petalWidth = 100.0
 
-struct Arc: InsettableShape {
-    let startAngle: Angle
-    let endAngle: Angle
-    let clockwise: Bool
-    var insetAmount = 0.0
-    
     func path(in rect: CGRect) -> Path {
-        let rotationAdjustment = Angle.degrees(90)
-        let modifiedStart = startAngle - rotationAdjustment
-        let modifiedEnd = endAngle - rotationAdjustment
-        
         var path = Path()
-        
-        path
-            .addArc(
-                center: CGPoint(x: rect.midX, y: rect.midY),
-                radius: rect.width / 2,
-                startAngle: startAngle,
-                endAngle: endAngle,
-                clockwise: !clockwise
-            )
-        
+
+        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
+            let rotation = CGAffineTransform(rotationAngle: number)
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width/2, y: rect.height/2))
+
+            let originalPetal = Path(ellipseIn: CGRect(x: petalOffest, y: 0, width: petalWidth, height: rect.width / 2))
+            let rotatedPetal = originalPetal.applying(position)
+
+            path.addPath(rotatedPetal)
+        }
+
         return path
-    }
-    
-    func inset(by amount: CGFloat) -> some InsettableShape {
-        var arc = self
-        arc.insetAmount += amount
-        return arc
     }
 }
 
 struct ContentView: View {
+    @State
+    private var petalOffset = -20.0
+    @State
+    private var petalWidth = 100.0
+
     var body: some View {
-        //        Triangle()
-        //            .fill(.red)
-        //            .frame(width: 300, height: 300)
-        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
-            .stroke(.blue, lineWidth: 40)
+        VStack {
+            Flower(petalOffest: petalOffset, petalWidth: petalWidth)
+                .fill(.yellow, style: FillStyle(eoFill: true))
+
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding([.horizontal, .bottom])
+
+            Text("Width")
+            Slider(value: $petalWidth, in: 0...100)
+                .padding(.horizontal)
+        }
     }
 }
